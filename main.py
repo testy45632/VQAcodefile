6a05f0afab8dedcd147da1f4f58fea636172db67
testy45632 <yourkeyk@icloud.com>
2024-07-18 15:35:37 +0900

    main.py

--- main.py
+++ main.py
@@ -115,13 +115,13 @@ class BasicBlock(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernelサイズ=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernelsize=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernelサイズ=1, stride=stride),
+                nn.Conv2d(in_channels, out_channels, kernelsize=1, stride=stride),
                 nn.BatchNorm2d(out_channels)
             )
     def forward(self, x):
@@ -136,17 +136,17 @@ class BottleneckBlock(nn.Module):
     expansion = 4
     def __init__(self, in_channels: int, out_channels: int, stride: int = 1):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernelサイズ=1, stride=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernelsize=1, stride=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernelサイズ=3, stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernelsize=3, stride=stride, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(in_channels, out_channels * self.expansion, kernelサイズ=1, stride=1)
+        self.conv3 = nn.Conv2d(in_channels, out_channels * self.expansion, kernelsize=1, stride=1)
         self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels * self.expansion:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels * self.expansion, kernelサイズ=1, stride=stride),
+                nn.Conv2d(in_channels, out_channels * self.expansion, kernelsize=1, stride=stride),
                 nn.BatchNorm2d(out_channels * self.expansion)
             )
     def forward(self, x):
@@ -162,10 +162,10 @@ class ResNet(nn.Module):
     def __init__(self, block, layers):
         super().__init__()
         self.in_channels = 64
-        self.conv1 = nn.Conv2d(3, 64, kernelサイズ=7, stride=2, padding=3)
+        self.conv1 = nn.Conv2d(3, 64, kernelsize=7, stride=2, padding=3)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernelサイズ=3, stride=2, padding=1)
+        self.maxpool = nn.MaxPool2d(kernelsize=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, layers[0], 64)
         self.layer2 = self._make_layer(block, layers[1], 128, stride=2)
         self.layer3 = self._make_layer(block, layers[2], 256, stride=2)
